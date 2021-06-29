0252c296f0818df02e6e853eadf17902cdc434ba
iyurev <idyurev@gmail.com>
2021-06-29 17:07:57 +0300

    pre release

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-modules-collection/ocpbooster/pkg/provider"
-	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 
 )
 
---
+++ tools/tools.go
@@ -0,0 +1,7 @@
+// +build tools
+package tools
+
+import (
+// document generation
+_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)

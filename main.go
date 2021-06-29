67276a7b3dc91fd0bd9d5fd9a6c903a9ccd77b1e
iyurev <idyurev@gmail.com>
2021-06-29 16:51:07 +0300

    + missing dep - github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-modules-collection/ocpbooster/pkg/provider"
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+
 )
 
 var (
